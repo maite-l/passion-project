07818dedacacbe3414ae764d492099a031480044
maite.l <maite.lejeune@student.howest.be>
2024-01-11 15:34:45 +0100

    exercises chapter 9

---
+++ exercises/js/09-03_move-x-and-y.js
@@ -0,0 +1,166 @@
+
+const vertexShaderRaw = `
+precision mediump float;
+
+attribute vec2 a_position;
+attribute vec2 a_texCoord;
+
+varying vec2 uv;
+
+void main() {
+    gl_Position = vec4(a_position, 1, 1);
+    uv = a_texCoord;
+}
+`;
+const fragmentShaderRaw = `
+precision mediump float;
+
+#define PI 3.14159265359
+
+uniform vec3 u_resolution;
+uniform float u_time;
+
+vec2 rotate2D(vec2 _st, float _angle){
+    _st -= 0.5;
+    _st =  mat2(cos(_angle),-sin(_angle),
+                sin(_angle),cos(_angle)) * _st;
+    _st += 0.5;
+    return _st;
+}
+
+vec2 brickTile(vec2 _st, float _zoom){
+    _st *= _zoom;
+
+    float time = 2.0 * step(1.0, mod((u_time), 2.0)) - 1.0;
+
+    if (time == 1.) {
+    	float row = 2.0 * step(1.0, mod(_st.y, 2.0)) - 1.0;
+    	_st.x += (row * time * u_time);
+    }
+    else {
+        float row = 2.0 * step(1.0, mod(_st.x, 2.0)) - 1.0;
+    	_st.y += (row * time * u_time);
+
+    }
+    return fract(_st);
+}
+
+float box(vec2 _st, vec2 _size){
+    _size = vec2(0.5)-_size*0.5;
+    vec2 uv = smoothstep(_size,_size+vec2(1e-4),_st);
+    uv *= smoothstep(_size,_size+vec2(1e-4),vec2(1.0)-_st);
+    return uv.x*uv.y;
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    vec3 color = vec3(0.267,0.347,0.605);
+
+    st = brickTile(st,5.0);
+    st = rotate2D(st,PI/2.);
+
+    vec3 boxes = vec3(box(rotate2D(st,PI/4.+u_time/2.),vec2(0.7)));
+
+    color = mix(color, vec3(0.650,1.000,0.454), boxes);
+
+    gl_FragColor = vec4(color,1.0);
+}
+`;
+
+const createShader = (gl, type, source) => {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+};
+
+const createProgram = (gl, vertexShader, fragmentShader) => {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+};
+
+const canvas = document.querySelector('#c');
+const gl = canvas.getContext('webgl');
+
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderRaw);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderRaw);
+
+const program = createProgram(gl, vertexShader, fragmentShader);
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+const positionBuffer = gl.createBuffer();
+const texCoordAttributeLocation = gl.getAttribLocation(program, "a_texCoord");
+const texCoordBuffer = gl.createBuffer();
+
+const u_resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+const u_timeLocation = gl.getUniformLocation(program, "u_time");
+
+const init = async () => {
+    gl.useProgram(program);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            -1.0, 1.0,  // uses clip space coordinates
+            1.0, 1.0,
+            -1.0, -1.0,
+
+            -1.0, -1.0,
+            1.0, 1.0,
+            1.0, -1.0
+        ]),
+        gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([  //different than the position buffer, because uses texture/image coordinates
+            0.0, 0.0,
+            1.0, 0.0,
+            0.0, 1.0,
+
+            0.0, 1.0,
+            1.0, 0.0,
+            1.0, 1.0,
+        ]),
+        gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(texCoordAttributeLocation);
+    gl.vertexAttribPointer(texCoordAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    canvas.width = 600;
+    canvas.height = 600;
+
+    gl.uniform3f(u_resolutionLocation, canvas.width, canvas.height, 0);
+
+    drawScene();
+};
+
+const drawScene = (time = 0) => {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.useProgram(program);
+
+    gl.uniform1f(u_timeLocation, time / 1000);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    requestAnimationFrame(drawScene);
+};
+
+init();
---
+++ exercises/js/09-06_truchet3.js
@@ -0,0 +1,183 @@
+
+const vertexShaderRaw = `
+precision mediump float;
+
+attribute vec2 a_position;
+attribute vec2 a_texCoord;
+
+varying vec2 uv;
+
+void main() {
+    gl_Position = vec4(a_position, 1, 1);
+    uv = a_texCoord;
+}
+`;
+const fragmentShaderRaw = `
+precision mediump float;
+
+#define PI 3.14159265359
+
+uniform vec3 u_resolution;
+uniform float u_time;
+
+vec2 rotate2D(vec2 _st, float _angle){
+    _st -= 0.5;
+    _st =  mat2(cos(_angle),-sin(_angle),
+                sin(_angle),cos(_angle)) * _st;
+    _st += 0.5;
+    return _st;
+}
+
+vec2 tile (vec2 _st, float _zoom) {
+    _st *= _zoom;
+    return fract(_st);
+}
+
+float circle(vec2 _st, float _radius){
+    vec2 pos = vec2(0.5)-_st;
+    return smoothstep(1.0-_radius,1.0-_radius+_radius*0.2,1.-dot(pos,pos)*3.14);
+}
+
+vec2 rotateTilePattern(vec2 _st){
+    _st *= 2.0;
+
+    float index = 0.0;
+    index += step(1., mod(_st.x,2.0));
+    index += step(1., mod(_st.y,2.0))*2.0;
+
+    _st = fract(_st);
+
+    if(index == 0.0){
+		_st = rotate2D(_st, PI/2.);
+    } else if(index == 2.0){
+		_st = rotate2D(_st, PI/2.);
+    }
+
+    return _st;
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+	vec3 color = vec3(1.000,0.984,0.748);
+
+
+    vec2 st1 = st;
+    st1 = tile(st1,4.0);
+    st1 = rotateTilePattern(st1);
+    st1 = rotateTilePattern(st1+2.);
+    st1 = rotate2D(st1,PI*abs(sin(u_time/2.)));
+
+    vec3 shape1Outside = vec3(circle(st1-vec2(0.49),1.0));
+    vec3 shape1Inside = vec3(circle(st1-vec2(0.49),0.65));
+    
+    vec3 shape2Outside = vec3(circle(st1+vec2(0.49),1.0));
+    vec3 shape2Inside = vec3(circle(st1+vec2(0.49),0.65));
+	
+    vec3 shape1 = vec3(shape1Outside-shape1Inside);
+    vec3 shape2 = vec3(shape2Outside-shape2Inside);
+    
+    vec3 shapes = shape1 + shape2;
+
+    color = mix(color, vec3(0.2,0.5,0.9), shapes);
+
+    gl_FragColor = vec4(color,1.0);
+}
+`;
+
+const createShader = (gl, type, source) => {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+};
+
+const createProgram = (gl, vertexShader, fragmentShader) => {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+};
+
+const canvas = document.querySelector('#c');
+const gl = canvas.getContext('webgl');
+
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderRaw);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderRaw);
+
+const program = createProgram(gl, vertexShader, fragmentShader);
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+const positionBuffer = gl.createBuffer();
+const texCoordAttributeLocation = gl.getAttribLocation(program, "a_texCoord");
+const texCoordBuffer = gl.createBuffer();
+
+const u_resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+const u_timeLocation = gl.getUniformLocation(program, "u_time");
+
+const init = async () => {
+    gl.useProgram(program);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            -1.0, 1.0,  // uses clip space coordinates
+            1.0, 1.0,
+            -1.0, -1.0,
+
+            -1.0, -1.0,
+            1.0, 1.0,
+            1.0, -1.0
+        ]),
+        gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([  //different than the position buffer, because uses texture/image coordinates
+            0.0, 0.0,
+            1.0, 0.0,
+            0.0, 1.0,
+
+            0.0, 1.0,
+            1.0, 0.0,
+            1.0, 1.0,
+        ]),
+        gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(texCoordAttributeLocation);
+    gl.vertexAttribPointer(texCoordAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    canvas.width = 600;
+    canvas.height = 600;
+
+    gl.uniform3f(u_resolutionLocation, canvas.width, canvas.height, 0);
+
+    drawScene();
+};
+
+const drawScene = (time = 0) => {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.useProgram(program);
+
+    gl.uniform1f(u_timeLocation, time / 1000);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    requestAnimationFrame(drawScene);
+};
+
+init();
