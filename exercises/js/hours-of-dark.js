1bc8a115a9f6e7626c7a57ebd16c93ab978b3afe
maite.l <maite.lejeune@student.howest.be>
2024-01-14 20:46:20 +0100

    exercises generative artistry

---
+++ exercises/js/hours-of-dark.js
@@ -0,0 +1,183 @@
+
+const vertexShaderRaw = `
+precision mediump float;
+
+attribute vec2 a_position;
+attribute vec2 a_texCoord;
+
+varying vec2 uv;
+
+void main() {
+    gl_Position = vec4(a_position, 1, 1);
+    uv = a_texCoord;
+}
+`;
+const fragmentShaderRaw = `
+precision mediump float;
+
+#define PI 3.14159265359
+
+uniform vec3 u_resolution;
+uniform float u_time;
+
+float box(in vec2 _st, in vec2 _size){
+    _size = vec2(0.5) - _size*0.5;
+    vec2 uv = smoothstep(_size, _size+vec2(0.001), _st);
+    uv *= smoothstep(_size, _size+vec2(0.001), vec2(1.0)-_st);
+    return uv.x*uv.y;
+}
+
+vec2 rotate2D (vec2 _st, float _angle) {
+    _st -= 0.5;
+    _st =  mat2(cos(_angle),-sin(_angle),
+                sin(_angle),cos(_angle)) * _st;
+    _st += 0.5;
+    return _st;
+}
+
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    vec3 color = vec3(0.);
+
+    vec2 border = vec2(4.,6.);
+    float columns = 23.;
+    float rows = 16.;
+    vec2 zoom = vec2(columns+border.x,rows+border.y);
+    vec2 size = vec2(0.3, 1.);
+
+    float offsetX = 1. / zoom.x * border.x/2.;
+    float offsetY = 1. / zoom.y * border.y/2.;
+
+    vec2 bl = step(vec2(offsetX,offsetY),st);
+	vec2 tr = step(vec2(offsetX,offsetY),1.0-st);
+    color += vec3((bl.x * bl.y * tr.x * tr.y));
+
+    st += vec2(-0.445,0.365);
+    color -= box(st, vec2((1./zoom.x)*3.+0.01, (1./zoom.y)*6.+0.01));
+    st -= vec2(-0.445, 0.365);
+
+    st.y = 1. - st.y;
+    st *= zoom;
+
+    vec2 ipos = floor(st);
+    vec2 fpos = fract(st);
+
+    st = fpos;
+
+    float cellIndex = (ipos.y+((ipos.x)*zoom.x));
+
+    float phi = (cellIndex/2. / (columns*rows)) * PI;
+	float theta = (sin(phi) * PI * 0.720);
+
+    st = rotate2D(st, theta);
+
+    float width = (abs(cos(phi))*0.4 + 0.1);
+
+    vec3 grid;
+    grid = vec3(box(st, vec2(width, 2.)))*vec3(1.);
+
+    color *= grid;
+    color *= mix(color, vec3(0.892,0.797,0.950), grid);
+
+	gl_FragColor = vec4(color,1.0);
+}
+`;
+
+const createShader = (gl, type, source) => {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+};
+
+const createProgram = (gl, vertexShader, fragmentShader) => {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+};
+
+const canvas = document.querySelector('#c');
+const gl = canvas.getContext('webgl');
+
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderRaw);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderRaw);
+
+const program = createProgram(gl, vertexShader, fragmentShader);
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+const positionBuffer = gl.createBuffer();
+const texCoordAttributeLocation = gl.getAttribLocation(program, "a_texCoord");
+const texCoordBuffer = gl.createBuffer();
+
+const u_resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+const u_timeLocation = gl.getUniformLocation(program, "u_time");
+
+const init = async () => {
+    gl.useProgram(program);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            -1.0, 1.0,  // uses clip space coordinates
+            1.0, 1.0,
+            -1.0, -1.0,
+
+            -1.0, -1.0,
+            1.0, 1.0,
+            1.0, -1.0
+        ]),
+        gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([  //different than the position buffer, because uses texture/image coordinates
+            0.0, 0.0,
+            1.0, 0.0,
+            0.0, 1.0,
+
+            0.0, 1.0,
+            1.0, 0.0,
+            1.0, 1.0,
+        ]),
+        gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(texCoordAttributeLocation);
+    gl.vertexAttribPointer(texCoordAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    canvas.width = 600;
+    canvas.height = 600;
+
+    gl.uniform3f(u_resolutionLocation, canvas.width, canvas.height, 0);
+
+    drawScene();
+};
+
+const drawScene = (time = 0) => {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.useProgram(program);
+
+    gl.uniform1f(u_timeLocation, time / 1000);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    requestAnimationFrame(drawScene);
+};
+
+init();
